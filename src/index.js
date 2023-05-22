a34c198ccf7af51e10f48a718bc31f17d65d0378
tomasrobino <tomaserobino@gmail.com>
2023-05-22 23:24:40 +0200

    switched two.js for pixi.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const { default: Two } = require("two.js");
+import * as PIXI from 'pixi.js';
+
 const { default: Pawn } = require("./Pawn");
 const { default: Rook } = require("./Rook");
 const { default: Knight } = require("./Knight");
 const { default: Bishop } = require("./Bishop");
 const { default: Queen } = require("./Queen");
 const { default: King } = require("./King");
+
+
 
 import "./styles.css";
 
@@ -77,84 +80,83 @@ window.onload = function() {
     //Graphics
     //-----
     const mainDiv = document.getElementById("mainDiv");
-    var two = new Two({fitted: true}).appendTo(mainDiv);
+    //var two = new Two({fitted: true}).appendTo(mainDiv);
+    var app = new PIXI.Application({ width: 480, height: 480, backgroundColor: 0xA52A2A });
+    document.body.appendChild(app.view);
+    
 
+    //Draw board squares    
     for (let i=0; i<8; i+=2) {
         for (let k=0; k<8; k+=2) {
-            let rect = two.makeRectangle(i*60 +90, k*60 +30, 60, 60);
-            rect.fill = "burlywood";
-            rect.noStroke();
+            let rect = PIXI.Sprite.from(PIXI.Texture.WHITE);
+            rect.tint = 0xDEB887;
+            rect.width = 60;
+            rect.height = 60;
+            rect.x = i*60 +60;
+            rect.y = k*60;
+            app.stage.addChild(rect);
 
-            let rect2 = two.makeRectangle(i*60 +30, k*60 +90, 60, 60);
-            rect2.fill = "burlywood";
-            rect2.noStroke();
+            let rect2 = PIXI.Sprite.from(PIXI.Texture.WHITE);
+            rect2.tint = 0xDEB887;
+            rect2.width = 60;
+            rect2.height = 60;
+            rect2.x = i*60;
+            rect2.y = k*60 +60;
+            app.stage.addChild(rect2);
         }
     }
 
+    //Drawn pieces' sprites
     for (let i=0; i<8; i++) {
         for (let k=0; k<8; k++) {
             let b = locBoard[i][k];
             let g;
-            if (b instanceof Pawn) {
-                g = two.makeRectangle(i*60 +30, k*60 +30, 40, 40);
-                if (b.player === "white") {
-                    g.fill = new Two.Texture(pawnWhite);
-                } else {
-                    g.fill = new Two.Texture(pawnBlack);
+            if (b !== 0) {
+                let sprite;
+                if (b instanceof Pawn) {
+                    if (b.player === "white") {
+                        sprite = PIXI.Sprite.from(pawnWhite);
+                    } else {
+                        sprite = PIXI.Sprite.from(pawnBlack);
+                    }
+                } else if (b instanceof Rook) {
+                    if (b.player === "white") {
+                        sprite = PIXI.Sprite.from(rookWhite);
+                    } else {
+                        sprite = PIXI.Sprite.from(rookBlack);
+                    }
+                } else if (b instanceof Knight) {
+                    if (b.player === "white") {
+                        sprite = PIXI.Sprite.from(knightWhite);
+                    } else {
+                        sprite = PIXI.Sprite.from(knightBlack);
+                    }
+                } else if (b instanceof Bishop) {
+                    if (b.player === "white") {
+                        sprite = PIXI.Sprite.from(bishopWhite);
+                    } else {
+                        sprite = PIXI.Sprite.from(bishopBlack);
+                    }
+                } else if (b instanceof Queen) {
+                    if (b.player === "white") {
+                        sprite = PIXI.Sprite.from(queenWhite);
+                    } else {
+                        sprite = PIXI.Sprite.from(queenBlack);
+                    }
+                } else if (b instanceof King) {
+                    if (b.player === "white") {
+                        sprite = PIXI.Sprite.from(kingWhite);
+                    } else {
+                        sprite = PIXI.Sprite.from(kingBlack);
+                    }
                 }
-                
-                g.noStroke();
-                b.graphic = g;
-            } else if (b instanceof Rook) {
-                g = two.makeRectangle(i*60 +30, k*60 +30, 40, 40);
-                if (b.player === "white") {
-                    g.fill = new Two.Texture(rookWhite);
-                } else {
-                    g.fill = new Two.Texture(rookBlack);
-                }
-                g.noStroke();
-                b.graphic = g;
-            } else if (b instanceof Knight) {
-                g = two.makeRectangle(i*60 +30, k*60 +30, 40, 40);
-                if (b.player === "white") {
-                    g.fill = new Two.Texture(knightWhite);
-                } else {
-                    g.fill = new Two.Texture(knightBlack);
-                }
-                g.noStroke();
-                b.graphic = g;
-            } else if (b instanceof Bishop) {
-                g = two.makeRectangle(i*60 +30, k*60 +30, 40, 40);
-                if (b.player === "white") {
-                    g.fill = new Two.Texture(bishopWhite);
-                } else {
-                    g.fill = new Two.Texture(bishopBlack);
-                }
-                g.noStroke();
-                b.graphic = g;
-            } else if (b instanceof Queen) {
-                g = two.makeRectangle(i*60 +30, k*60 +30, 40, 40);
-                if (b.player === "white") {
-                    g.fill = new Two.Texture(queenWhite);
-                } else {
-                    g.fill = new Two.Texture(queenBlack);
-                }
-                g.noStroke();
-                b.graphic = g;
-            } else if (b instanceof King) {
-                g = two.makeRectangle(i*60 +30, k*60 +30, 40, 40);
-                if (b.player === "white") {
-                    g.fill = new Two.Texture(kingWhite);
-                } else {
-                    g.fill = new Two.Texture(kingBlack);
-                }
-                g.noStroke();
-                b.graphic = g;
+
+                sprite.x = i*60 +10;
+                sprite.y = k*60 +10;
+                app.stage.addChild(sprite);
             }
         }
     }
-    
-    two.update();
 }
 
 export var board = locBoard;
