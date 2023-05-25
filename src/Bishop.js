d378ad70b746c122eddac5044a213bb18895c6f9
tomasrobino <tomaserobino@gmail.com>
2023-05-25 18:08:03 +0200

    bishop movement done

--- src/Bishop.js
+++ src/Bishop.js
@@ -1,5 +1,64 @@
+import { board, setBoard } from ".";
+
 const { default: Piece } = require("./Piece");
 
 export default class Bishop extends Piece {
-    
+    checkMove() {
+        let list = [];
+        let sel;
+
+        for (let i = 1; i < 8-this.y && i < 8-this.x; i++) {
+            sel = board[this.y+i][this.x+i];
+            if (sel instanceof Piece && sel.player === this.player) {
+                break;
+            } else if (sel === 0) {
+                setBoard(this.y+i, this.x+i, 1);
+                list.push([this.y+i, this.x+i]);
+            } else {
+                this.targets.push([this.y+i, this.x+i]);
+                break;
+            }
+        }
+        
+        for (let i = 1; i < 8-this.y && i < this.x+1; i++) {
+            sel = board[this.y+i][this.x-i];
+            if (sel instanceof Piece && sel.player === this.player) {
+                break;
+            } else if (sel === 0) {
+                setBoard(this.y+i, this.x-i, 1);
+                list.push([this.y+i, this.x-i]);
+            } else {
+                this.targets.push([this.y+i, this.x-i]);
+                break;
+            }
+        }
+        
+        for (let i = 1; i < 8-this.x && i < this.y+1; i++) {
+            sel = board[this.y-i][this.x+i];
+            if (sel instanceof Piece && sel.player === this.player) {
+                break;
+            } else if (sel === 0) {
+                setBoard(this.y-i, this.x+i, 1);
+                list.push([this.y-i, this.x+i]);
+            } else {
+                this.targets.push([this.y-i, this.x+i]);
+                break;
+            }
+        }
+        
+        for (let i = 1; i < this.x+1 && i < this.y+1; i++) {
+            sel = board[this.y-i][this.x-i];
+            if (sel instanceof Piece && sel.player === this.player) {
+                break;
+            } else if (sel === 0) {
+                setBoard(this.y-i, this.x-i, 1);
+                list.push([this.y-i, this.x-i]);
+            } else {
+                this.targets.push([this.y-i, this.x-i]);
+                break;
+            }
+        }
+        
+        return list;
+    }
 }
